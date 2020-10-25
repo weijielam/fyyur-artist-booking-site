b845a7a63bc9fca33541c1e9c04e43f1ea825a30
Wei Jie Lam <weijielam@gmail.com>
2020-10-25 11:42:51 +0000

    fixed issues with new_artist form and create_artist_submission() which did not allow creation of new artists

--- app.py
+++ app.py
@@ -132,6 +132,7 @@ def venues():
   for venue in venues:
     upcoming_shows = []
     if venue_city_and_state == venue.city + venue.state:
+      data[len(data) - 1]["venues"].append
       print('match')
     else:
       venue_city_and_state == venue.city + venue.state
@@ -150,9 +151,6 @@ def venues():
     print(venue.state)
     print(venue.city)
     print(data)
-    
-    
-    
 
   return render_template('pages/venues.html', areas=data);
 
@@ -260,7 +258,7 @@ def show_venue(venue_id):
 #  ----------------------------------------------------------------
 
 @app.route('/venues/create', methods=['GET'])
-def create_venue_form():
+def create_ ():
   form = VenueForm()
   return render_template('forms/new_venue.html', form=form)
 
@@ -308,16 +306,24 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  # data=[{
+  #   "id": 4,
+  #   "name": "Guns N Petals",
+  # }, {
+  #   "id": 5,
+  #   "name": "Matt Quevedo",
+  # }, {
+  #   "id": 6,
+  #   "name": "The Wild Sax Band",
+  # }]
+
+  data = []
+  artists = Artist.query.all() 
+  print(artists)
+
+  for artist in artists:
+    print(artist.id)
+    print(artist.name)
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
@@ -477,14 +483,30 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
+  # TODO: insert form data as a new Artist record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
 
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
+  try:
+    new_artist = Artist(
+      name = request.form['name'],
+      city = request.form['city'],
+      state = request.form['state'],
+      phone = request.form['name'],
+      genres= request.form.getlist('genres'),
+      image_link = request.form['image_link'],
+      facebook_link = request.form['facebook_link'],
+    )
+    print(new_artist)
+    db.session.add(new_artist)
+    db.session.commit()
+    # on successful db insert, flash success
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+  except:
+    print(sys.exc_info())
+    flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.')
   # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
+  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
   return render_template('pages/home.html')
 
 
